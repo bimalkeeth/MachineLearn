8f02a80944cd3f527f25546c1206c41084a1e633
Bimalkeeth <bkaluarachchi@assetic.com>
2018-10-19 12:55:03 +1100

    plotting

---
+++ ChaptorOne/Example_06.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonum/stat"
+	"github.com/kniren/gota/dataframe"
+	"gonum.org/v1/gonum/floats"
+	"log"
+	"os"
+)
+
+func main() {
+	irisFile, err := os.Open("./CData/iris_labeled.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer irisFile.Close()
+
+	irisDF := dataframe.ReadCSV(irisFile)
+	sepalLength := irisDF.Col("petal_length").Float()
+	minVal := floats.Min(sepalLength)
+	maxVal := floats.Max(sepalLength)
+	rangeVal := maxVal - minVal
+
+	varianceVal := stat.Variance(sepalLength, nil)
+	stdDevVal := stat.StdDev(sepalLength, nil)
+	inds := make([]int, len(sepalLength))
+	floats.Argsort(sepalLength, inds)
+	//Get the Quantiles
+	quant25 := stat.Quantile(0.25, stat.Empirical, sepalLength, nil)
+	quant50 := stat.Quantile(0.50, stat.Empirical, sepalLength, nil)
+	quant75 := stat.Quantile(0.75, stat.Empirical, sepalLength, nil)
+
+	// Output the results to standard out.
+	fmt.Printf("\nSepal Length Summary Statistics:\n")
+	fmt.Printf("Max value: %0.2f\n", maxVal)
+	fmt.Printf("Min value: %0.2f\n", minVal)
+	fmt.Printf("Range value: %0.2f\n", rangeVal)
+	fmt.Printf("Variance value: %0.2f\n", varianceVal)
+	fmt.Printf("Std Dev value: %0.2f\n", stdDevVal)
+	fmt.Printf("25 Quantile: %0.2f\n", quant25)
+	fmt.Printf("50 Quantile: %0.2f\n", quant50)
+	fmt.Printf("75 Quantile: %0.2f\n", quant75)
+
+}
