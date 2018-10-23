fb464c378bb657057fcbfb3f56e269615825d78f
Bimalkeeth <bkaluarachchi@assetic.com>
2018-10-23 17:14:49 +1100

    plotting

---
+++ ChaptorOne/Example_12.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/integrate"
+	"gonum.org/v1/gonum/stat"
+)
+
+func main() {
+	scores := []float64{0.1, 0.35, 0.4, 0.8}
+	classes := []bool{true, false, true, false}
+
+	// Calculate the true positive rates (recalls) and
+	// false positive rates.
+	tpr, fpr := stat.ROC(0, scores, classes, nil)
+	// Compute the Area Under Curve.
+	auc := integrate.Trapezoidal(fpr, tpr)
+	// Output the results to standard out.
+	fmt.Printf("true positive rate: %v\n", tpr)
+	fmt.Printf("false positive rate: %v\n", fpr)
+	fmt.Printf("auc: %v\n", auc)
+}
